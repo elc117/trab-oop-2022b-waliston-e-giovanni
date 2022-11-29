18802616ae96290b9afc1a0cf28d13c4d1d97129
Waliston Euripedes <82295321+wal-wizard@users.noreply.github.com>
2022-11-29 16:12:58 -0300

    Delete magicCards directory

--- magicCards/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.dev.magiccards {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.dev.magiccards to javafx.fxml;
-    exports com.dev.magiccards;
-}
